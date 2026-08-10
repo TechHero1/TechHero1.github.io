b52f120938ea1375e3aee16913b58e2c0cf647bc
Tech <19197222+TechHero1@users.noreply.github.com>
2026-08-10 03:18:32 -0300

    wp-chunk / get alliance images test

--- wp-chunk/script.js
+++ wp-chunk/script.js
@@ -23,17 +23,19 @@ function getImage(set_url=false) {
         document.querySelector(".chunk-newtab-btn").href = url;
 
         if (set_url) window.location.search = `?method=${method}&x=${x}&y=${y}`;
+        return
     }
     if (method == "hq") {
         let hq_id = document.querySelector("#hq-id").value;
         let url = `https://backend.wplace.live/alliances/${hq_id}/headquarters/image`;
-        
+
         if (hq_id == "") return
 
         document.querySelector(".img-preview").src = url;
         document.querySelector(".hq-newtab-btn").href = url;
 
         if (set_url) window.location.search = `?method=${method}&id=${hq_id}`;
+        return
     }
 }
 
@@ -52,4 +54,17 @@ function changeMethod(sel_method) {
     document.querySelector(".hq-form").classList.add("hidden");
 
     document.querySelector("."+method+"-form").classList.remove("hidden");
+}
+
+function test_get_alliance_images() {
+    let alliance_id = document.querySelector("#hq-id").value;
+
+    if (method == "hq" && alliance_id != "") {
+        document.querySelector(".hq-form").innerHTML += `
+            <div class="flex flex-row gap-2 w-[100%] items-center">
+                <div class="w-[50%]"><center><img src="https://backend.wplace.live/alliances/${alliance_id}/assets/picture"></center></div>
+                <div class="w-[50%]"><center><img src="https://backend.wplace.live/alliances/${alliance_id}/assets/banner"></center></div>
+            </div>
+        `;
+    }
 }
